1f19df2d0bd48203ad4146155265c17b0b029ab9
hanmariyang <hanmari.code@gmail.com>
2022-10-11 22:06:37 +0900

    [생성/업데이트] Home/urls, views 연결
    1) urls - 생성 후 views 파일이랑 연결
    2) views - MainHome 함수 생성(home.html 연결)
    3) urls <-> views 연결 완료

--- Mytaste_SNS/urls.py
+++ Mytaste_SNS/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Home.views import
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path()
+    path('', include('Home.urls')),
 ]
