4c0eac874e63284909ed4377ee9ef31d70220960
hanmariyang <hanmari.code@gmail.com>
2022-10-12 20:21:23 +0900

    [삭제] account APP 이름 수정으로 삭제
    1)  account -> accounts 변경
    2) account 앱이름이 allauth 랑 충돌하는 이유로 앱이름 변경

--- account/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def sign_up_view(request):
-    return render(request, 'account/sign_up.html')
-
-def sign_in_view(request):
-    return render(request, 'account/sign_in.html')
