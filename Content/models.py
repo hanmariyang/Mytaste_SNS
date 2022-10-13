1498803182f77897dd07b5e10f2c727900c149f4
hanmariyang <hanmari.code@gmail.com>
2022-10-13 21:17:35 +0900

    [생성] FeedModel DB 필드
    author = models.ForeignKey(UserModel, on_delete=models.CASCADE) -> UserModel 에서 정보 가져오기
    content = models.CharField(max_length=256) -> 피드 내용
    created_at = models.DateTimeField(auto_now_add=True) -> 생성 일자
    updated_at = models.DateTimeField(auto_now=True) -> 수정 일자
    image = models.TextField() -> 피드 이미지
    like_count = models.IntegerField() -> 좋아요 숫자

--- Content/models.py
+++ Content/models.py
@@ -1,3 +1,16 @@
+from email.mime import image
 from django.db import models
+from accounts.models import UserModel
+
 
 # Create your models here.
+class FeedModel(models.Model):
+    class Meta:
+        db_table = "feed"
+
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.TextField()
+    like_count = models.IntegerField()
